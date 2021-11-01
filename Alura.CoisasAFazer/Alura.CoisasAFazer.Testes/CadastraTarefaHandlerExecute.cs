1c2b4297403dfcc867a375a5d6a9cf6d5ce1fe1e
Karolina Gomes <karolina_bp@hotmail.com>
2021-11-01 17:47:41 -0300

    Testando qual mensagem de Log vem no metodo de incluir tarefa

--- Alura.CoisasAFazer/Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
+++ Alura.CoisasAFazer/Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
@@ -7,6 +7,8 @@ using System;
 using System.Linq;
 using Xunit;
 using Moq;
+using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
 
 namespace Alura.CoisasAFazer.Testes
 {
@@ -21,12 +23,16 @@ namespace Alura.CoisasAFazer.Testes
         [Fact]
         public void IncluiNoBdDadaTarefaValida()
         {
-            //Para criar nossa tarefa
+            //Para criar nova tarefa
             //Criar comando
             //Executar o comando
 
             //arrange
             var comando = new CadastraTarefa("Estudar xUnit", new Categoria("Estudo"), new DateTime(2021, 12, 31));
+
+            //Usar o mock para passar uma instancia de ILogger
+            var mock = new Mock<ILogger<CadastraTarefaHandler>>();
+            var log = mock.Object;
 
             var options = new DbContextOptionsBuilder<DbTarefasContext>()
                 .UseInMemoryDatabase("DbTarefas")
@@ -36,7 +42,7 @@ namespace Alura.CoisasAFazer.Testes
             var repo = new RepositorioTarefa(context);
 
             //tratador do comando acima
-            var handler = new CadastraTarefaHandler(repo);
+            var handler = new CadastraTarefaHandler(repo, log);
 
             //act
             handler.Execute(comando); //System Under Test (SUT - Sistema sob teste) -> CadastraTarefaHandlerExecute
@@ -64,8 +70,12 @@ namespace Alura.CoisasAFazer.Testes
             //Instancia do tipo simulado
             var repo = mock.Object; //Dá um objeto pra gente daquilo tipo passado na instancia do mock
 
+            //Usar o mock para passar uma instancia de ILogger
+            var mockLogger = new Mock<ILogger<CadastraTarefaHandler>>();
+            var log = mockLogger.Object;
+
             //tratador do comando acima
-            var handler = new CadastraTarefaHandler(repo);
+            var handler = new CadastraTarefaHandler(repo, log);
 
             //act
             CommandResult resultado = handler.Execute(comando); //System Under Test (SUT - Sistema sob teste) -> CadastraTarefaHandlerExecute
@@ -74,6 +84,103 @@ namespace Alura.CoisasAFazer.Testes
 
             //Assert.True(true);
             Assert.False(resultado.IsSuccess); //Verifica se e false
+        }
+
+        //Dado exceção lancada
+        //Quando chamar método execute
+        //Logar mensagem da exceção
+
+        [Fact]
+        public void DadoExceptionLancadaLogarMensagemExecao()
+        {
+            //arrange
+            var mensagemErro = "Houve um erro na inclusão de tarefas";
+
+            var excecao = new Exception(mensagemErro);
+
+            var comando = new CadastraTarefa("Estudar xUnit", new Categoria("Estudo"), new DateTime(2021, 12, 31));
+
+            //Ao invés de usar o bd em memória, eu posso usar o mock
+            var mock = new Mock<IRepositorioTarefas>();
+
+            //Simula um repositorio que lança uma excecao no metodo incluirtarefas
+            //Aqui faz as configurações como lançamento de exceção
+            mock.Setup(obj => obj.IncluirTarefas(It.IsAny<Tarefa[]>()))//It.IsAny == qualquer array de tarefa recebido ele vai lançar exceção
+                .Throws(excecao);
+
+            //Instancia do tipo simulado
+            var repo = mock.Object; //Dá um objeto pra gente daquilo tipo passado na instancia do mock
+
+            //Usar o mock para passar uma instancia de ILogger
+            var mockLogger = new Mock<ILogger<CadastraTarefaHandler>>();
+            var log = mockLogger.Object;
+
+            //tratador do comando acima
+            var handler = new CadastraTarefaHandler(repo, log);
+
+            //act
+            CommandResult resultado = handler.Execute(comando);
+
+            //assert
+            //Log error e um metodo de extensao e nao da para fazer verificações ou setups com moq usando metodos de extensao
+            //Pra isso vamos ter que usar o metodo mesmo que seria o Log, não o de extensao
+            mockLogger.Verify(log => 
+            log.Log(
+                    LogLevel.Error, //Nível de log (todos os niveis tem metodo que falicitam mas o mock nao aceita) => LogError
+                    mensagemErro, //Id do Evento
+                    It.IsAny<EventId>(), //Id do Evento
+                    It.IsAny<Object>(), //Objeto que será logado, no caso seria a excecao, mas já estou pegando ela em baixo
+                    excecao, //exceca que sera logada
+                    It.IsAny<Func<object, Exception, string>>()//Funcao Converte objeto e excecao numa string
+                ), 
+                Times.Once());
+        }
+
+        delegate void CapturaMensagemLog(LogLevel level, EventId eventId, object state, Exception exception, Func<object, Exception, string> function);
+
+        [Fact]
+        public void IncluiNoBdDadaTarefa()
+        {
+            //arrange
+            var tituloTarefaEsperado = "Usar Moq para aprofundar conhecimento de API";
+            var comando = new CadastraTarefa(tituloTarefaEsperado, new Categoria("Estudo"), new DateTime(2021, 12, 31));
+
+            //Usar o mock para passar uma instancia de ILogger
+            var mockLogger = new Mock<ILogger<CadastraTarefaHandler>>();
+
+            LogLevel levelCapturado = LogLevel.Error;
+            string mensagemCapturada = string.Empty;
+
+            CapturaMensagemLog captura = (level, eventId, state, exception, func) =>
+            {
+                levelCapturado = level;
+                mensagemCapturada = func(state, exception);
+            };
+
+            //Quero capturar todas as vezes que esse método for chamado
+            mockLogger.Setup(l => 
+            l.Log(
+                    It.IsAny<LogLevel>(),
+                    It.IsAny<EventId>(), 
+                    It.IsAny<Object>(), 
+                    It.IsAny<Exception>(), 
+                    It.IsAny<Func<object, Exception, string>>()
+                )).Callback(captura);
+
+            var log = mockLogger.Object;
+
+            var mockRepo = new Mock<IRepositorioTarefas>();
+            var repo = mockRepo.Object;
+
+            //tratador do comando acima
+            var handler = new CadastraTarefaHandler(repo, log);
+
+            //Act
+            handler.Execute(comando);
+
+            //Assert
+            Assert.Equal(LogLevel.Debug, levelCapturado);
+            Assert.Contains(tituloTarefaEsperado, mensagemCapturada); //verificar se contém sub string dentro de string
         }
     }
 }
